573c88e9ff657afcdf4983cd87cd8a8ae3b0ff57
Leonardo <leonardoes2025@gmail.com>
2025-10-29 11:34:29 -0300

    v1.6.1

--- pagamentos-para-woocommerce-com-appmax.php
+++ pagamentos-para-woocommerce-com-appmax.php
@@ -14,7 +14,7 @@
  * Description:       Habilita os métodos de pagamentos do gateway de pagamentos da AppMax.
  * Requires at least: 4.0
  * Requires PHP:      7.2
- * Version:           1.6.0
+ * Version:           1.6.1
  * Author:            AppMax
  * Author URI:        https://appmax.com.br/
  * License:           GPLv3 or later
